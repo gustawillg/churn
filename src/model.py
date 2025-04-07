da28bb8df672f3391b43a25e08a9b2eeb1a84b01
Gustavo <122057283+gustawillg@users.noreply.github.com>
2025-04-06 23:36:42 -0300

    chore

--- src/model.py
+++ src/model.py
@@ -1,18 +1,33 @@
 import mlflow
 import mlflow.sklearn
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 import joblib
 
+def load_and_preprocess_data():
+    print("Carregando os dados...")
+    data = pd.read_csv("dados/dados_clientes.csv")
+
+    print("Pré-processando os dados...")
+    X = data.drop("churn", axis=1)
+    y = data["churn"]
+
+    X = X.apply(lambda col: col.astype('float64') if col.dtype == 'int64' else col)
+
+    return X, y
+
 def train_and_evaluate(X, y):
+    print("Treinando o modelo...")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
+
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test) 
-    acc = accuracy_score(y_test, y_pred)  
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
     print("Relatório de Classificação:")
     print(classification_report(y_test, y_pred))
     print("Acurácia:", acc)
@@ -21,9 +36,16 @@ def train_and_evaluate(X, y):
     mlflow.log_param("random_state", 42)
     mlflow.log_metric("accuracy", acc)
 
-    model_path = "modelo_churn.pkl"
-    joblib.dump(model, model_path)  
+    input_example = pd.DataFrame([X_test.iloc[0]])
+    mlflow.sklearn.log_model(model, "model", input_example=input_example)
 
-    mlflow.sklearn.log_model(model, "model")
+    joblib.dump(model, "modelo_churn.pkl")
 
     return model
+
+def main():
+    X, y = load_and_preprocess_data()
+    train_and_evaluate(X, y)
+
+if __name__ == "__main__":
+    main()
